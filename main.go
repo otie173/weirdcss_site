a3db5c052c2f018bf870a0f18c33d0cba8ac6028
otie173 <otie173@192.168.100.9>
2024-08-03 21:44:32 +0300

    added css file with styles (weirdcss CSS Framework)

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 	fileServer := http.FileServer(http.Dir("static"))
 	http.Handle("static/", http.StripPrefix("static", fileServer))
 
-	if err := http.ListenAndServe("localhost:8080", nil); err != nil {
+	if err := http.ListenAndServe("0.0.0.0:8080", nil); err != nil {
 		log.Fatalln(err)
 	}
 }
